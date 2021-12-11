778b42ed6d9de06cbfe08b25acd22870ccf2ebf9
georgiy.bondarenko <bondarenko.georgiy@skyeng.ru>
2021-12-11 12:24:44 +0300

    Initial Commit

---
+++ Habits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habits
+//
+//  Created by Георгий Бондаренко on 11.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
